6edf39c6b1bd75261e089b226bceb3dd7a6ea6ca
vaiibhav75 <vaiibhav75@gmail.com>
2023-01-15 09:57:28 +0000

    Sync LeetCode submission - First Missing Positive (java)

--- LeetCode/problems/first_missing_positive/solution.java
+++ LeetCode/problems/first_missing_positive/solution.java
@@ -1,16 +1,37 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        int[] arr = new int[nums.length];
+
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] <= nums.length && nums[i] >0) {
-                arr[nums[i]-1] = nums[i];
+
+            // looping until we find the correct value 
+            while (nums[i] != i+1) {
+                
+                // skipping out of range values
+                if (nums[i] >= nums.length || nums[i] <= 0) {
+                    break;
+                }
+                // skipping duplicates
+                if (nums[i] == nums[nums[i]-1]) {
+                    break;
+                }
+                // swapping with correct value
+                swap(nums , i , nums[i]-1);
             }
         }
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i]==0){
-                return i + 1;
+
+        // returning the missing positive value
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i] != i+1) {
+                return i+1;
             }
         }
-        return arr.length +1;
+        return nums.length + 1;
+    }
+
+    // swap function
+    static void swap (int[] nums, int i, int j) {
+        int k = nums[i];
+        nums[i] = nums[j];
+        nums[j] = k;
     }
 }
