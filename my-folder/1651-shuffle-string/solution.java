57d27784a15bda08163f46ae55c7a5458f9dc6d2
vaiibhav75 <vaiibhav75@gmail.com>
2023-01-23 08:23:18 +0000

    [LeetCode Sync] Runtime - 1 ms (74.89%), Memory - 42.3 MB (99.96%)

---
+++ my-folder/1651-shuffle-string/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String restoreString(String s, int[] indices) {
+        char[] arr = new char[indices.length];
+        for (int i = 0; i < indices.length; i++) {
+            arr[indices[i]] = s.charAt(i);
+        }
+        StringBuilder myString = new StringBuilder();
+        for (int i = 0; i < indices.length; i++) {
+            myString.append(arr[i]);
+        }
+        return myString.toString();
+        
+    }
+}
