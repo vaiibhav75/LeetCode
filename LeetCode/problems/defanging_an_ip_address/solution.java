9deb0f70c0d8160c6425a4b07c6e724949a37148
vaiibhav75 <vaiibhav75@gmail.com>
2023-01-23 08:08:59 +0000

    Sync LeetCode submission - Defanging an IP Address (java)

---
+++ LeetCode/problems/defanging_an_ip_address/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String defangIPaddr(String address) {
+        StringBuilder myString = new StringBuilder();
+        for (int i = 0; i < address.length(); i++) {
+            if (address.charAt(i) == '.') {
+                myString.append("[.]");
+            } else {
+                myString.append(address.charAt(i));
+            }
+        }
+        return myString.toString();
+        
+    }
+}
