06094f0694a8abaac759072e2ab74e3959595d16
vaiibhav75 <vaiibhav75@gmail.com>
2023-03-02 09:57:54 +0000

    Sync LeetCode submission - Fibonacci Number (java)

--- LeetCode/problems/fibonacci_number/solution.java
+++ LeetCode/problems/fibonacci_number/solution.java
@@ -1,9 +1,5 @@
 class Solution {
     public int fib(int n) {
-        if (n <= 1) {
-            return n;
-        } else {
-            return fib(n-1) + fib(n-2);
-        }
+        return (int) ((Math.pow(( (1 + Math.sqrt(5)) / 2 ), n) -Math.pow(( (1 - Math.sqrt(5)) / 2 ), n) )/ Math.sqrt(5));
     }
 }
