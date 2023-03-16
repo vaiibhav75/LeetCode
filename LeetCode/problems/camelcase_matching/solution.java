adcb75f4dad1c70bd4a059d8ae158b4f145e44f8
vaiibhav75 <vaiibhav75@gmail.com>
2023-03-16 05:49:20 +0000

    Sync LeetCode submission - Camelcase Matching (java)

---
+++ LeetCode/problems/camelcase_matching/solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<Boolean> camelMatch(String[] queries, String pattern) {
+        int k = 0;
+        List<Boolean> ans = new ArrayList<>();
+        for (int i = 0; i < queries.length; i++) {
+            ans.add(camelCheck(queries[i],pattern ));
+        }
+        return ans;
+    }
+    
+    static boolean camelCheck (String query, String pattern) {
+        int k = 0;
+
+        for (int i = 0; i < query.length(); i++) {
+            if (k < pattern.length() && pattern.charAt(k) == query.charAt(i)) {
+                k++;
+            } else {
+                if (query.charAt(i) >= 'A' && query.charAt(i) <= 'Z') {
+                    return false;
+                }
+            }
+        }
+        return k == pattern.length();
+    }
+}
+
