516a9e94218acb4397c3d808ab1902db3e16071d
vaiibhav75 <vaiibhav75@gmail.com>
2023-03-17 09:48:14 +0000

    [LeetCode Sync] Runtime - 1 ms (86.15%), Memory - 39.9 MB (99.61%)

--- my-folder/0069-sqrtx/solution.java
+++ my-folder/0069-sqrtx/solution.java
@@ -7,18 +7,15 @@ class Solution {
 
         int start = 1;
         int end = x/2;
-        int ans = 0;
 
         while (start <= end) {
             int mid = start + (end-start)/2;
             if (mid <= x/mid) {
-                ans = mid;
                 start = mid +1;
-                
             } else {
                 end = mid - 1;
             }
         }
-        return ans;
+        return end;
     }
 }
