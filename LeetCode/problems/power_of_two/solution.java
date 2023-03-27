03661ccc66b610eb5c03ce9f46d116691a24f232
vaiibhav75 <vaiibhav75@gmail.com>
2023-03-27 18:00:18 +0000

    Sync LeetCode submission - Power of Two (java)

--- LeetCode/problems/power_of_two/solution.java
+++ LeetCode/problems/power_of_two/solution.java
@@ -3,6 +3,12 @@ class Solution {
         if (n <= 0) {
             return false;
         }
-        return ((n & (n-1)) == 0);
+        if (n == 1) {
+            return true;
+        }
+        if (n %2 != 0) {
+            return false;
+        }
+        return (isPowerOfTwo(n/2));
     }
 }
