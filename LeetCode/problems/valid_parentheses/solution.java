430faa971c0357151a7e9df6828355ecafd8e85c
vaiibhav75 <vaiibhav75@gmail.com>
2023-03-28 16:22:07 +0000

    Sync LeetCode submission - Valid Parentheses (java)

--- LeetCode/problems/valid_parentheses/solution.java
+++ LeetCode/problems/valid_parentheses/solution.java
@@ -1,57 +1,45 @@
 class Solution {
     public boolean isValid(String s) {
-        char firstOpen = 0;
-        char secondOpen = 0;
-        char thirdOpen = 0;
-        boolean b1Open = false;
-        boolean b2Open = false;
-        boolean b3Open = false;
-
-        StringBuilder ans = new StringBuilder();
-
-        char currBrac = 0;
-        for (int i = 0; i < s.length() ; i++) {
-            currBrac = s.charAt(i);
-            switch (currBrac) {
-                case '(', '{', '[' -> {
-                    ans.append(currBrac);
-                }
-                case ')'-> {
-                    if (ans.length() == 0) {
-                        return false;
-                    }
-                    char last = ans.charAt(ans.length()-1);
-                    if (last == '('){
-                        ans.deleteCharAt(ans.length()-1);
-                    } else {
-                        return false;
-                    }
-                }
-                case  '}'-> {
-                    if (ans.length() == 0) {
-                        return false;
-                    }
-                    char last = ans.charAt(ans.length()-1);
-                    if (last == '{'){
-                        ans.deleteCharAt(ans.length()-1);
-                    } else {
-                        return false;
-                    }
-                }
-                case  ']'-> {
-                    if (ans.length() == 0) {
-                        return false;
-                    }
-                    char last = ans.charAt(ans.length()-1);
-                    if (last == '['){
-                        ans.deleteCharAt(ans.length()-1);
-                    } else {
-                        return false;
-                    }
-                }
-
+        StringBuilder str = new StringBuilder(s);
+        return solve(str);
+    }
+    
+    static boolean solve (StringBuilder str) {
+        if (str.length() == 0) {
+            return true;
+        }
+        int k = 0;
+        char x = '0';
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c == '(' || c == '{' || c == '['){
+               x = c;
+               k = i;
             }
         }
-        return (ans.length() == 0);
+        if (x == '0' ) {
+            return false;
+        }
+        
+        if (k == str.length()-1) {
+            return false;
+        }
+        
+        if (x == '(') {
+            x = ')';
+        }
+        if (x == '[') {
+            x = ']';
+        }
+        if (x == '{') {
+            x = '}';
+        }
+        
+        if (x == str.charAt(k+1)) {
+            str.deleteCharAt(k);
+            str.deleteCharAt(k);
+            return solve(str);
+        }
+        return false;
     }
 }
