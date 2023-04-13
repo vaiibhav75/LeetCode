fe22c16339c53b946a6ce665dc6a189a307643ae
vaiibhav75 <vaiibhav75@gmail.com>
2023-04-13 08:12:51 +0000

    Sync LeetCode submission - Search in Rotated Sorted Array (java)

--- LeetCode/problems/search_in_rotated_sorted_array/solution.java
+++ LeetCode/problems/search_in_rotated_sorted_array/solution.java
@@ -1,50 +1,38 @@
 class Solution {
-    public int search(int[] nums, int target) {
-      
-        int start,end;
-        if (target <= nums[nums.length-1]) {
-            start = rotationMeasure(nums);
-            end = nums.length-1;
-        } else {
-            start = 0;
-            end = rotationMeasure(nums) - 1;
-        }
 
-        while (start <= end) {
-            int mid = (start + end)/2;
-
-            if (nums[mid] > target) {
-                end = mid - 1;
-            } else if (nums[mid] < target) {
-                start = mid + 1;
-            } else {
-                return mid;
-            }
-        }
-        return -1;
-
+    public static void main(String[] args) {
+        int[] arr = {4,5,6,7,0,1,2};
+        System.out.println(search(arr,0));
+    }
+    static int search(int[] nums, int target) {
+        return solve(nums,target,0, nums.length-1);
     }
 
-    static int rotationMeasure (int[] nums) {
-
-
-        int start = 1;
-        int end = nums.length - 1;
-
-        if (nums[end] > nums[0]) {
-            return 0;
+    static int solve (int[] nums, int target, int s, int e) {
+        if (s > e) {
+            return -1;
         }
-        while (start <= end) {
-            int mid = (start + end)/2;
+        int mid = s +(e-s)/2;
 
-            if (nums[mid] < nums[mid-1]) {
-                return mid;
-            } else if (nums[mid] >= nums[0]) {
-                start = mid +1 ;
-            } else if (nums[mid] <= nums[nums.length-1]) {
-                end = mid - 1;
+        if (nums[mid] == target) {
+            return mid;
+        }
+
+        if (nums[s] <= nums[mid]) {
+            if (target >= nums[s] && target <= nums[mid]){
+                return solve(nums,target,s, mid-1);
+            } else {
+                return solve(nums,target,mid+1, e);
             }
         }
-        return 0;
+        if (nums[e] >= nums[mid]){
+            if (target <= nums[e] && target >= nums[mid]){
+                return solve(nums,target,mid+1, e);
+            } else {
+                return solve(nums,target,s, mid-1);
+            }
+        }
+        
+        return -1;
     }
 }
