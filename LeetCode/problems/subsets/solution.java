1b24ff19961633560d09d9418e42f97164d00292
vaiibhav75 <vaiibhav75@gmail.com>
2023-06-08 06:25:51 +0000

    Sync LeetCode submission - Subsets (java)

--- LeetCode/problems/subsets/solution.java
+++ LeetCode/problems/subsets/solution.java
@@ -1,17 +1,27 @@
 class Solution {
-    public List<List<Integer>> subsets(int[] nums) {
+
+//    public static void main(String[] args) {
+//        int[] a = {1,2,3,4,5,6,7,8};
+//        List<List<Integer>> ans = subsets(Arrays.);
+//        System.out.println(Collections.unmodifiableList(ans));
+//    }
+    static List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        for (int i = 0; i < (1<< nums.length); i++) {
-            List<Integer> arr = new ArrayList<>();
-            int j = 0;
-            while (j < 12) {
-                if (((i>>j) & 1) == 1) {
-                    arr.add(nums[j]);
-                }
-                j++;
+        List<Integer> p = new ArrayList<>();
+        ans.add(p);
+
+        for (int i = 0; i < nums.length; i++) {
+            int n = ans.size();
+            int x = nums[i];
+
+            for (int j = 0; j < n; j++) {
+                List<Integer> curr = new ArrayList<>(ans.get(j));
+                curr.add(x);
+                ans.add(curr);
             }
-            ans.add(arr);
         }
         return ans;
     }
+
+    
 }
