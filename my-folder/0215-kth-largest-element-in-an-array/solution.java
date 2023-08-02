95e3def0d03fd09391e94b3fb1f5024b81c79c4e
vaiibhav75 <vaiibhav75@gmail.com>
2023-08-02 18:17:42 +0000

    [LeetCode Sync] Runtime - 14 ms (93.61%), Memory - 54.1 MB (99.83%)

--- my-folder/0215-kth-largest-element-in-an-array/solution.java
+++ my-folder/0215-kth-largest-element-in-an-array/solution.java
@@ -1,6 +1,51 @@
 class Solution {
+
     public int findKthLargest(int[] nums, int k) {
-        Arrays.sort(nums);
+        sort(nums,0,nums.length-1, nums.length-k);
         return nums[nums.length - k];
     }
+
+
+
+    static void sort (int[] arr, int i, int j, int target) {
+        if (i >= j) {
+            return;
+        }
+        int anchor = i;
+        int x = i+1;
+        int y = j;
+
+        while (x <= y) {
+            while (x <= j && arr[x] <= arr[anchor] ) {
+                x++;
+            }
+
+            while ( y >= i && arr[y] > arr[anchor] ) {
+                y--;
+            }
+
+            if (x <= y) {
+                int temp = arr[x];
+                arr[x] = arr[y];
+                arr[y] = temp;
+            } else {
+                break;
+            }
+        }
+
+        int temp = arr[anchor];
+        arr[anchor] = arr[y];
+        arr[y] = temp;
+ 
+        if (y < target) {
+            sort(arr,y+1,j,target);
+        } else if (y > target) {
+            sort(arr,i,y-1,target);
+        } else {
+            return;
+        }
+
+    }
+
+
 }
