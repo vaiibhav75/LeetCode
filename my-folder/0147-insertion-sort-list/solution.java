e4221b3663edd5fa0a06814971a5504655ac05bf
vaiibhav75 <vaiibhav75@gmail.com>
2023-10-15 08:18:47 +0000

    [LeetCode Sync] Runtime - 18 ms (53.98%), Memory - 43.8 MB (96.32%)

---
+++ my-folder/0147-insertion-sort-list/solution.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode insertionSortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        
+        ListNode sorted = insertionSortList(head.next);
+        
+        if (head.val <= sorted.val) {
+            head.next = sorted;
+            return head;
+        }
+        
+        ListNode curr = sorted;
+        
+        while (curr.next != null && head.val > curr.next.val) {
+            curr = curr.next;
+        }
+        
+       
+        head.next = curr.next;
+        curr.next = head;
+        
+        return sorted;
+        
+        
+        
+    }
+}
+
