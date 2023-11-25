cf32a96476b5d0b0fc093d8fd4e9c4d68f74a3f2
vaiibhav75 <vaiibhav75@gmail.com>
2023-11-25 16:52:29 +0000

    [LeetCode Sync] Runtime - 1 ms (100.00%), Memory - 45.2 MB (99.48%)

--- my-folder/0004-median-of-two-sorted-arrays/solution.java
+++ my-folder/0004-median-of-two-sorted-arrays/solution.java
@@ -1,144 +1,44 @@
 class Solution {
-    
-    static double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int totLength = nums1.length + nums2.length;
-
-        if (totLength %2 == 0) {
-            double answer = 0;
-            int median2 = totLength/2;
-            int median1 = totLength/2 -1;
-            ArrayList<Integer> arr = new ArrayList<>();
-
-            int lo = 0;
-            int hi = nums1.length-1;
-
-            while (hi >= lo) {
-
-                int mid = lo + (hi - lo)/2;
-                int position = posFinder(nums2, nums1[mid]) + mid;
-                if ( position == median1 || position == median2) {
-                    arr.add(nums1[mid]);
-                    if (position == median1){
-                        lo = mid+1;
-                    } else {
-                        hi = mid-1;
-                    }
-
-                } else if (position > median2) {
-                    hi = mid-1;
-                } else if (position < median1) {
-                    lo = mid + 1;
-                }
-            }
-            if (arr.size() == 2) {
-                answer = (arr.get(0) + arr.get(1));
-                answer = answer/2;
-                return answer;
-            }
-            lo = 0;
-            hi = nums2.length-1;
-            while (hi >= lo) {
-                int mid = lo + (hi - lo)/2;
-                int position = posFinder2(nums1, nums2[mid]) + mid;
-                if ( position == median1 || position == median2) {
-                    arr.add(nums2[mid]);
-                    if (position == median1){
-                        lo = mid+1;
-                    } else {
-                        hi = mid-1;
-                    }
-                } else if (position > median2) {
-                    hi = mid-1;
-                } else if (position < median1) {
-                    lo = mid + 1;
-                }
-            }
-
-            answer = (arr.get(0) + arr.get(1));
-            answer = answer/2;
-            return answer;
-        } else {
-            int median = totLength/2;
-            int lo = 0;
-            int hi = nums1.length-1;
-            while (hi >= lo) {
-                int mid = lo + (hi - lo)/2;
-                int position = posFinder(nums2, nums1[mid]) + mid;
-                if ( position == median) {
-                    return (double) nums1[mid];
-                } else if (position > median) {
-                    hi = mid-1;
-                } else {
-                    lo = mid + 1;
-                }
-            }
-
-            lo = 0;
-            hi = nums2.length-1;
-            while (hi >= lo) {
-                int mid = lo + (hi - lo)/2;
-                int position = posFinder2(nums1, nums2[mid]) + mid;
-                if ( position == median) {
-                    return (double) nums2[mid];
-                } else if (position > median) {
-                    hi = mid-1;
-                } else {
-                    lo = mid + 1;
-                }
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        int m = nums2.length;
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        int x =n+m;
+        int f = x/2;
+        int [] arr = new int [x];
+        while(i+j<x && i < n && j < m){
+            if(nums1[i]<nums2[j]){
+                arr[k]=nums1[i];
+                k++;
+                i++;
+            }else{
+                arr[k]=nums2[j];
+                k++;
+                j++;
             }
         }
-        return 0;
+
+        while (i < n) {
+            arr[k]=nums1[i];
+            k++;
+            i++;
+        }
+
+        while (j < m) {
+            arr[k]=nums2[j];
+            k++;
+            j++;
+        }
+       
+
+        if(x%2==0){
+            double a = (double) ((double)arr[f - 1] + (double) arr[f]) /2;
+            
+            return a;
+        }else{
+            return (double) arr[f];
+        }
     }
-
-    static int posFinder (int[] arr, int target) {
-        if (arr.length == 0) {
-            return 0;
-        }
-        int lo = 0;
-        int hi =arr.length-1;
-        if (target > arr[hi]) {
-            return hi + 1;
-        }
-        if (target < arr[lo]) {
-            return 0;
-        }
-
-        while (hi >= lo) {
-            int mid = (lo + hi) / 2;
-
-            if (target < arr[mid]) {
-                hi = mid - 1;
-            } else {
-                lo = mid + 1;
-            }
-        }
-        return lo;
-    }
-    static int posFinder2 (int[] arr, int target) {
-        if (arr.length == 0) {
-            return 0;
-        }
-        int lo = 0;
-        int hi =arr.length-1;
-        if (target < arr[lo]) {
-            return 0;
-        }
-        if (target > arr[hi]) {
-            return hi + 1;
-        }
-
-        while (hi >= lo) {
-            int mid = (lo + hi) / 2;
-
-            if (target <= arr[mid]) {
-                hi = mid - 1;
-            } else {
-                lo = mid + 1;
-            }
-        }
-        return hi+1;
-    }
-
 }
-
-
