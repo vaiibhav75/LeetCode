d5ddecf235624f8b3a54fe72e653b4c7f2c921e7
vaiibhav75 <vaiibhav75@gmail.com>
2024-02-26 18:20:55 +0000

    [LeetCode Sync] Runtime - 22 ms (43.96%), Memory - 54.1 MB (48.28%)

---
+++ my-folder/0969-number-of-recent-calls/solution.java
@@ -0,0 +1,24 @@
+class RecentCounter {
+    Queue<Integer> queue;
+    public RecentCounter() {
+        queue = new LinkedList<>();
+        
+    }
+
+    public int ping(int t) {
+        while (!queue.isEmpty() && queue.peek() < t -3000) {
+            queue.remove();
+        }
+        
+        queue.add(t);
+        
+        return queue.size();
+        
+    }
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter();
+ * int param_1 = obj.ping(t);
+ */
