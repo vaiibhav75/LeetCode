d900f795e7855d549cefc2d56cec56bc30189338
vaiibhav75 <vaiibhav75@gmail.com>
2024-02-26 16:22:10 +0000

    Sync LeetCode submission - Valid Parentheses (java)

--- LeetCode/problems/valid_parentheses/solution.java
+++ LeetCode/problems/valid_parentheses/solution.java
@@ -1,45 +1,30 @@
 class Solution {
     public boolean isValid(String s) {
-        StringBuilder str = new StringBuilder(s);
-        return solve(str);
-    }
-    
-    static boolean solve (StringBuilder str) {
-        if (str.length() == 0) {
-            return true;
-        }
-        int k = 0;
-        char x = '0';
-        for (int i = 0; i < str.length(); i++) {
-            char c = str.charAt(i);
-            if (c == '(' || c == '{' || c == '['){
-               x = c;
-               k = i;
+        Stack<Character> stack = new Stack<>();
+        for (char c: s.toCharArray()) {
+            if (c == '[' || c == '(' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char x = stack.pop();
+                if (x == '(') {
+                    x = ')';
+                }
+                if (x == '[') {
+                    x = ']';
+                }
+                if (x == '{') {
+                    x = '}';
+                }
+                
+                if (c != x) {
+                    return false;
+                }
             }
         }
-        if (x == '0' ) {
-            return false;
-        }
         
-        if (k == str.length()-1) {
-            return false;
-        }
-        
-        if (x == '(') {
-            x = ')';
-        }
-        if (x == '[') {
-            x = ']';
-        }
-        if (x == '{') {
-            x = '}';
-        }
-        
-        if (x == str.charAt(k+1)) {
-            str.deleteCharAt(k);
-            str.deleteCharAt(k);
-            return solve(str);
-        }
-        return false;
+        return stack.isEmpty();
     }
 }
