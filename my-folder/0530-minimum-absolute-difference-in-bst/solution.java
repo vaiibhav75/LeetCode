568006a84b36743dda373c23e31f8e92f38a678f
vaiibhav75 <vaiibhav75@gmail.com>
2024-03-13 08:17:11 +0000

    [LeetCode Sync] Runtime - 1 ms (48.03%), Memory - 44.3 MB (66.16%)

---
+++ my-folder/0530-minimum-absolute-difference-in-bst/solution.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int getMinimumDifference(TreeNode root) {
+        List<Integer> list = inorderTraversal(root);
+        int min = Integer.MAX_VALUE;
+        
+        for (int i = 0; i < list.size()-1; i++) {
+            min = Math.min(min, list.get(i+1) - list.get(i));
+        }
+        return min;
+    }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        traverse(root,result);
+        return result;
+    }
+
+    void traverse (TreeNode node, List<Integer> ans) {
+        if (node == null) return;
+        traverse(node.left,ans);
+        ans.add(node.val);
+        traverse(node.right,ans);
+
+    }
+}
