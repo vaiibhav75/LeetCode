29bf1a988df2bc04249ec683ea399d63b09887a3
vaiibhav75 <vaiibhav75@gmail.com>
2024-04-19 11:31:13 +0000

    [LeetCode Sync] Runtime - 2 ms (95.94%), Memory - 45.7 MB (36.57%)

---
+++ my-folder/0637-average-of-levels-in-binary-tree/solution.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> ans = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        
+        while (!queue.isEmpty()) {
+            double currSize = queue.size();
+            double sum = 0;
+
+            for (int i = 0; i < currSize; i++) {
+                TreeNode curr = queue.poll();
+                sum += curr.val;
+                if (curr.left != null) queue.add(curr.left);
+                if (curr.right != null) queue.add(curr.right);
+            }
+            
+            ans.add(sum/currSize);
+        }
+        
+        return ans;
+    }
+}
