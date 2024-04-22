77228ee521b01cf4662a73baec4c61d246f39b92
vaiibhav75 <vaiibhav75@gmail.com>
2024-04-22 17:40:53 +0000

    [LeetCode Sync] Runtime - 31 ms (16.73%), Memory - 45.6 MB (76.57%)

--- my-folder/0297-serialize-and-deserialize-binary-tree/solution.java
+++ my-folder/0297-serialize-and-deserialize-binary-tree/solution.java
@@ -10,14 +10,46 @@
 public class Codec {
 
     // Encodes a tree to a single string.
-    public TreeNode serialize(TreeNode root) {
+    public String serialize(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        buildString(root, sb);
+        return sb.toString();
+    }
+    
+    public void buildString(TreeNode root, StringBuilder sb) {
+        if (root == null) {
+            sb.append("# ");
+            return;
+        }
+        sb.append(root.val + " ");
+        buildString(root.left, sb);
+        buildString(root.right, sb);
+    }
+
+   
+
+    
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        String[] traverse = data.split(" ");
+        ArrayList<String> list = new ArrayList<>(Arrays.asList(traverse));
+        return buildTree(list);
+    }
+    
+    public TreeNode buildTree(ArrayList<String> list) {
+        if (list.get(0).equals("#")) {
+            list.remove(0);
+            return null;
+        }
+        TreeNode root = new TreeNode(Integer.parseInt(list.get(0)));
+        list.remove(0);
+        root.left = buildTree(list);
+        root.right = buildTree(list);
         return root;
     }
 
-    // Decodes your encoded data to tree.
-    public TreeNode deserialize(TreeNode data) {
-        return data;
-    }
+    
 }
 
 // Your Codec object will be instantiated and called as such:
