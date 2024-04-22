29170d0109cccc489605f7c38de3de6edcd9310c
vaiibhav75 <vaiibhav75@gmail.com>
2024-04-22 17:42:49 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.9 MB (57.06%)

--- my-folder/0112-path-sum/solution.java
+++ my-folder/0112-path-sum/solution.java
@@ -16,12 +16,7 @@
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
         if (root == null) return false;
-        return hasPathSum1(root, targetSum); 
-    }
-
-    public boolean hasPathSum1(TreeNode root, int targetSum) {
-        if (root == null) return false;
         if (root.left == null && root.right == null && targetSum == root.val) return true;
-        return hasPathSum1(root.left, targetSum - root.val) || hasPathSum1(root.right, targetSum - root.val); 
+        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val); 
     }
 }
