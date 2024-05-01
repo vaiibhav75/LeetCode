59f40c24e2816bda2ab0d0b227285d076cf6941d
vaiibhav75 <vaiibhav75@gmail.com>
2024-05-01 08:26:41 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42 MB (50.61%)

--- my-folder/0103-binary-tree-zigzag-level-order-traversal/solution.java
+++ my-folder/0103-binary-tree-zigzag-level-order-traversal/solution.java
@@ -1,36 +1,3 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode() {
-    }
-
-    TreeNode(int val) {
-        this.val = val;
-    }
-
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
-
-class Carry {
-    int height;
-    TreeNode node;
-    public Carry (int height, TreeNode node) {
-        this.height = height;
-        this.node = node;
-    }
-}
-
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> ans = new ArrayList<>();
@@ -46,16 +13,8 @@ class Solution {
             for (int i = 0; i < size; i++) {
                 TreeNode x = list.poll();
                 curr.add(x.val);
-
-                // if (swap) {
-                //     if (x.right != null) list.add(x.right);
-                //     if (x.left != null) list.add(x.left);
-                    
-                // } else {
-                    if (x.left != null) list.add(x.left);
-                    if (x.right != null) list.add(x.right);
-                    
-                // }
+                if (x.left != null) list.add(x.left);
+                if (x.right != null) list.add(x.right);
             }
             if (swap) Collections.reverse(curr);
             ans.add(curr);
